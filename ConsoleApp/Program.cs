5c0704689829daee3af1092c48a3dde84f85dda1
MuhammadHuzaifa812 <huziendo@gmail.com>
2026-05-30 14:35:13 +0500

    Add project files.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿String sometext = "Hello, World!";
+
+List<string> textlist = new List<string>();
+
+textlist.Add(sometext);
+textlist.Add("Hammad Rahil");
+
+Console.WriteLine(textlist[0]);
+Console.WriteLine(textlist[1]);
+Console.ReadLine();
