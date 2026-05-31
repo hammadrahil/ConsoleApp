dabee5fdf999d7a065d2ac867ea90064d3c0509e
Hammad Rahil <289158596+hammadrahil@users.noreply.github.com>
2026-06-01 02:32:10 +0500

    Update ContextFactory.cs

--- Csharp.Entites/ContextFactory.cs
+++ Csharp.Entites/ContextFactory.cs
@@ -5,8 +5,7 @@ namespace Csharp.Entites
 {
     public class ContextFactory : IDesignTimeDbContextFactory<Context>
     {
-        public static string ConnectionString = "Server=localhost;Port=3306;Database=CarCompanyDB;User=root;Password=HuziEndo@123;Connection Timeout=30;SslMode=Preferred;";
-
+        public static string ConnectionString = "Server=localhost;Port=3306;Database=CarCompanyDB;User=root;Password=YOUR_PASSWORD;SslMode=Preferred;";
         public Context CreateDbContext(string[] args)
         {
             var optionsBuilder = new DbContextOptionsBuilder<Context>();
